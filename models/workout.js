9a17ad233bc5f6421c27e8b445c55bcf26fc3590
Jennifer Davila <pazjenni1331@gmail.com>
2021-09-07 12:31:54 -0500

    updated server.js and model file - need to modify model file to get workout data

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+module.exports = router;
